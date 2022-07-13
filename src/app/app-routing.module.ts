729507aad086a9173e949902ee6b16375a9e428f
Lars Christian Ersly <lce@bankdata.dk>
2022-07-13 16:49:51 +0200

    Add admin module and firebase+firebaseui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {PageThanksComponent} from "./features/question/special-pages/page-thanks
 import {QuestionPageComponent} from "./features/question/question-page/question-page.component";
 
 const routes: Routes = [
+  {path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)},
   {
     path: '', component: QuestionRootComponent, children: [
       {path: '', pathMatch: 'full', redirectTo: 'start'},
