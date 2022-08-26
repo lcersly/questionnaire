a0bacbce2d12d3e6b4be2314ef84242e1875f9d1
Lars Christian Ersly <lce@bankdata.dk>
2022-08-26 09:14:01 +0200

    Add firebase config for prod and emulator data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyAX26rs0kqHX5II8KhUlVvUETrLW2eiYBU",
+    authDomain: "questionnaire-test-b3636.firebaseapp.com",
+    projectId: "questionnaire-test-b3636",
+    storageBucket: "questionnaire-test-b3636.appspot.com",
+    messagingSenderId: "95548162383",
+    appId: "1:95548162383:web:71f69abbeed10371b7c53a",
+    measurementId: "G-VT1JP623MM"
+  },
   useEmulators: false,
 };
